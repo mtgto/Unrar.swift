f997530b210f1c9630bb505b9dd39c4db2f1549b
Dani Garcia <fillito@gmail.com>
2023-04-03 15:24:12 +0200

    Add testCanCreateEntry_withDateHeader
    Co-authored-by: mtgto <hogerappa@gmail.com>

--- Tests/UnrarTests/EntryTests.swift
+++ Tests/UnrarTests/EntryTests.swift
@@ -16,7 +16,13 @@ final class EntryTests: XCTestCase {
         // Then
         XCTAssertEqual(entry.modified, Date(timeIntervalSince1970: 0))
     }
-
+      func testCanCreateEntry_withDateHeader() {
+        var header = RARHeaderDataEx()
+        header.MtimeLow = 848753920
+        header.MtimeHigh = 29986355
+        let entry = Entry(header)
+        XCTAssertEqual(entry.modified, Date(timeIntervalSince1970: 1234567890))
+    }
     static var allTests = [
         ("testCanCreateEntry_withDateHeaderZero", testCanCreateEntry_withDateHeaderZero),
     ]
